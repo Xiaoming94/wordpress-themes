0f583ad82db935c7472a9b55b48c7a2c24055cc5
xiaoming <henyan94@gmail.com>
2016-12-15 11:52:09 +0100

    corrected permalinks stuff

--- killme/index.php
+++ killme/index.php
@@ -3,7 +3,7 @@
 get_header();
 if (have_posts()) :
   while (have_posts()) : the_post(); ?>
-      <h2><a href='<?php the_permalink?>'><?php the_title();?></a></h2>
+      <h2><a href='<?php the_permalink();?>'><?php the_title();?></a></h2>
       <p>
         <?php the_content(); ?>
       </p>
