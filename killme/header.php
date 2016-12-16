f77ff53284732cb8563ed4ebdd1710d2c47839c6
xiaoming <henyan94@gmail.com>
2016-12-16 15:08:02 +0100

    Changed some layout stuff with the header

--- killme/header.php
+++ killme/header.php
@@ -7,12 +7,13 @@
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-  <div class="container">
-    <header class="site-header">
-      <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
-      <h5><?php bloginfo('description'); ?></h5>
-    </header>
-
+  <div class="page-header">
+    <div class="container">
+      <header class="site-header">
+        <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+        <h5><?php bloginfo('description'); ?></h5>
+      </header>
+    </div>
   </div>
   <div class="container">
     <nav>
