5c7cb1845b84155f5181df7c3b08d26d7082bfa5
xiaoming <henyan94@gmail.com>
2016-12-20 13:57:50 +0100

    Added a comments.php file and starting to work with comments

---
+++ killme/comments.php
@@ -0,0 +1,3 @@
+<?php if(!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME'])) : ?>
+	<?php die('You can not access this page directly!'); ?>
+<?php endif; ?>
