b748c4db5d5150e57a74edf19e162b3dcd503088
xiaoming <henyan94@gmail.com>
2017-04-12 16:33:46 +0200

    Added Search form input box to searchform.php

--- killme/searchform.php
+++ killme/searchform.php
@@ -2,7 +2,8 @@
   <label for="s">Seach:</label>
   <form id="searchform" method="get" action="/index.php">
       <div>
-          
+          <input type="text" name="s" id="s" size="15" />
+          <input type="submit" value="Search" />
       </div>
   </form>
 </li>
