5333c23272b0d9b45b6217b9294bb697026b5d80
xiaoming <henyan94@gmail.com>
2017-05-09 17:49:27 +0200

    Added style to Searchform.php

--- killme/searchform.php
+++ killme/searchform.php
@@ -1,9 +1,16 @@
 <li id="search">
-  <label for="s">Seach:</label>
   <form id="searchform" method="get" action="/index.php">
+    <div class="search-form form-group">
       <div>
-          <input type="text" name="s" id="s" size="15" />
-          <input type="submit" value="Search" />
+        <div class="input-group">
+          <input class="form-control" type="text" name="s" id="s" size="15" placeholder="search" />
+          <div class="input-group-btn">
+            <button class="btn btn-default search-button" type="submit">
+              <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+            </button>
+          </div>
+        </div>
       </div>
+    </div>
   </form>
 </li>
