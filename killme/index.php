f9417246af2dd50119bcd7a9e7685e615de96918
xiaoming <henyan94@gmail.com>
2017-05-22 08:21:20 +0200

    Fixed some indentations in the code

--- killme/index.php
+++ killme/index.php
@@ -14,36 +14,36 @@
         <div class="container-fluid">
           <?php
           $content = get_the_content();
-          if (str_word_count( strip_tags( $content ))>76):
-            $trimmed = wp_trim_words($content, 76, '...');
+          if (str_word_count( strip_tags( $content ))>200):
+            $trimmed = wp_trim_words($content, 200, '...');
             echo $trimmed . "\n"; ?>
             <div class="continue-button-div">
               <a href='<?php the_permalink(); ?>'
                 class="btn btn-default continue-btn">Continue Reading</a>
-              </div>
-              <?php
-              else :
-                the_content();
-              endif;
-              ?>
             </div>
-          </p>
-          <div class="post-panel container-fluid">
-            <ul class="post-panel-links">
-              <li><?php the_category(); ?></li>
-              <li>
-                <span class="glyphicon glyphicon-comment" aria-hidden="true"></span>
-                <?php comments_number("0","1","%"); ?>
-              </li>
-            </ul>
-          </div>
-        <?php endwhile;
-        else :
-          echo '<p>no content found :(</p>';
-        endif;
+          <?php
+          else :
+            the_content();
+          endif;
+              ?>
+        </div>
+      </p>
+      <div class="post-panel container-fluid">
+        <ul class="post-panel-links">
+          <li><?php the_category(); ?></li>
+          <li>
+            <span class="glyphicon glyphicon-comment" aria-hidden="true"></span>
+            <?php comments_number("0","1","%"); ?>
+          </li>
+        </ul>
+      </div>
+    <?php endwhile;
+      else :
+        echo '<p>no content found :(</p>';
+      endif;
 
         ?>
-  </div>
+    </div>
 
 
 <?php get_footer(); ?>
