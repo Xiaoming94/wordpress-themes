ab205832c968d321adce62a63f86699e9a105279
Henry Yang <henyan94@gmail.com>
2017-11-14 10:53:52 +0100

    Deleted killme theme

--- themes/killme/config.rb
+++
@@ -1,8 +0,0 @@
-http_path = "/"
-css_dir = "."
-sass_dir = "sass"
-images_dir = "images"
-javascript_dir = "js"
-relative_assets = true
-extensions_dir = "bower_components"
-output_style = :compressed
--- themes/killme/sidebar-primary.php
+++
@@ -1,8 +0,0 @@
-<div id="sidebar-primary" class="sidebar">
-  <?php if (is_active_sidebar( 'sidebar-1' ) ): ?>
-    <?php dynamic_sidebar( 'sidebar-1' );?>
-  <?php else:?>
-    <!-- TODO: Think about default widgets later -->
-  <?php endif; ?>
-
-</div>
