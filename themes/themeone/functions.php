438284e0e516364e7044c762e66a87798a178db7
Xiaoming <mail@xiaoming.se>
2017-12-18 21:46:08 +0100

    Add JS directory

--- themes/themeone/functions.php
+++ themes/themeone/functions.php
@@ -3,6 +3,8 @@
   // enqueing assets
   function themeone_init_assets() {
     wp_enqueue_style('themeone-style', get_stylesheet_uri() );
+    wp_enqueue_script( 'script', get_template_directory_uri() .
+                        '/js/scripts.js', array ( 'jquery' ), 1.1, true );
   }
   add_action('wp_enqueue_scripts', 'themeone_init_assets');
 ?>
