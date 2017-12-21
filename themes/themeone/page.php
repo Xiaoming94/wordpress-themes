797bb4a398a8f04065c87283c1610a24a0dd813a
Xiaoming <mail@xiaoming.se>
2017-12-21 19:28:24 +0100

    Add page.php, and single.php with initial contents

--- themes/themeone/page.php
+++ themes/themeone/page.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * The index file of ThemeOne - Wordpress theme by XiaoMing
- * This theme is developed using PureScript and Sass with Foundation
+ * The page.php of ThemeOne - Wordpress theme by XiaoMing
+ * The content of this is for rendering single pages
  *
  * @link https://github.com/Xiaoming94/wordpress-theme/themes/themeone
  * @version 0.1.0
@@ -12,7 +12,7 @@
     if( have_posts() ):
       while( have_posts() ): the_post();?>
 
-        <h2><a href=<?php the_permalink(); ?>><?php the_title(); ?></a></h2>
+        <h2><?php the_title(); ?></h2>
         <?php
           the_content();
         ?>
