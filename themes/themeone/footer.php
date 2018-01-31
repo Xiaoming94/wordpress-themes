a4bf02976fcba316cb0f93c495758b4a49cc14b2
Xiaoming <mail@xiaoming.se>
2018-01-31 11:09:16 +0100

    Make footer menu vertical

--- themes/themeone/footer.php
+++ themes/themeone/footer.php
@@ -6,7 +6,8 @@
     <nav>
       <?php
         $args = array(
-          'theme_location' => 'footer'
+          'theme_location' => 'footer',
+          'menu_class'     => 'vertical menu'
         );
 
         wp_nav_menu( $args );
