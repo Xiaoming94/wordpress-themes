cf5e3202d12ef63484c8e99e815d25a1ee4fae7a
Xiaoming <mail@xiaoming.se>
2018-03-09 16:04:36 +0100

    Add Sidebar to theme

--- themes/themeone/functions.php
+++ themes/themeone/functions.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * function.php of ThemeOne - Wordpress theme by XiaoMing
+ * Contains actions made on the theme and codes for features
+ * Probably the only file written in purely PHP
+ */
 
   // enqueing assets
   function themeone_init_assets() {
@@ -7,6 +12,7 @@
     // Scripts
     wp_enqueue_script( 'script', get_template_directory_uri() . '/js/scripts.js', array ( 'jquery' ), 1.1, true );
   }
+
   // Register theme locations for Navigation menus
   function nav_menu_locations() {
     $menu_location = array(
@@ -16,9 +22,25 @@
     return $menu_location;
   }
 
+  // Creating Sidebars for the theme.
+  function create_sidebar() {
+    $sidebar_args = array (
+      'name'          => __('Widgets Sidebar', 'themeone'),
+      'id'            => 'sidebar-1',
+      'description'   => __('Widgets to be displayed on the index page','themeone')
+    );
+    register_sidebar($sidebar_args);
+  }
+
+  // Register Nav-menu
   register_nav_menus(nav_menu_locations());
+
   // Loading the assets
   add_action('wp_enqueue_scripts', 'themeone_init_assets');
+
+  // Load the sidebar to the theme
+  add_action('widgets_init','create_sidebar');
+
   // Let Wordpress manage document title
   // This is literally to not use <title>
   add_theme_support('title-tag');
---
+++ themes/themeone/sidebar.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Sidebar.php of ThemeOne - A wordpress theme by XiaoMing
+ * Template file for the sidebars of the theme
+ */
+?>
+
+<div id="sidebar" class="widget-area" role="complementary" aria-label="<?php esc_attr_e( 'Widgets Sidebar', 'themeone' ); ?>">
+  <?php dynamic_sidebar( 'sidebar-1' ); ?>
+</div>
