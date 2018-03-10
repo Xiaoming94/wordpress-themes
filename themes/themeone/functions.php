f2143f2adac578252111f543a694dc66253eb932
Xiaoming <mail@xiaoming.se>
2018-03-10 21:27:20 +0100

    indentations

--- themes/themeone/functions.php
+++ themes/themeone/functions.php
@@ -5,45 +5,44 @@
  * Probably the only file written in purely PHP
  */
 
-  // enqueing assets
-  function themeone_init_assets() {
-    // Styles
-    wp_enqueue_style('themeone-style', get_stylesheet_uri() );
-    // Scripts
-    wp_enqueue_script( 'script', get_template_directory_uri() . '/js/scripts.js', array ( 'jquery' ), 1.1, true );
-  }
+// enqueing assets
+function themeone_init_assets() {
+  // Styles
+  wp_enqueue_style('themeone-style', get_stylesheet_uri() );
+  // Scripts
+  wp_enqueue_script( 'script', get_template_directory_uri() . '/js/scripts.js', array ( 'jquery' ), 1.1, true );
+}
 
-  // Register theme locations for Navigation menus
-  function nav_menu_locations() {
-    $menu_location = array(
-      'primary' => __( 'Primary Menu' ),
-      'footer'  => __( 'Footer Menu' )
-    );
-    return $menu_location;
-  }
+// Register theme locations for Navigation menus
+function nav_menu_locations() {
+  $menu_location = array(
+    'primary' => __( 'Primary Menu' ),
+    'footer'  => __( 'Footer Menu' )
+  );
+  return $menu_location;
+}
 
-  // Creating Sidebars for the theme.
-  function create_sidebar() {
-    $sidebar_args = array (
-      'name'          => __('Widgets Sidebar', 'themeone'),
-      'id'            => 'sidebar-1',
-      'description'   => __('Widgets to be displayed on the index page','themeone')
-    );
-    register_sidebar($sidebar_args);
-  }
+// Creating Sidebars for the theme.
+function create_sidebar() {
+  $sidebar_args = array (
+    'name'          => __('Widgets Sidebar', 'themeone'),
+    'id'            => 'sidebar-1',
+    'description'   => __('Widgets to be displayed on the index page','themeone')
+  );
+  register_sidebar($sidebar_args);
+}
 
-  // Register Nav-menu
-  register_nav_menus(nav_menu_locations());
+// Register Nav-menu
+register_nav_menus(nav_menu_locations());
 
-  // Loading the assets
-  add_action('wp_enqueue_scripts', 'themeone_init_assets');
+// Loading the assets
+add_action('wp_enqueue_scripts', 'themeone_init_assets');
 
-  // Load the sidebar to the theme
-  add_action('widgets_init','create_sidebar');
+// Load the sidebar to the theme
+add_action('widgets_init','create_sidebar');
 
-  // Let Wordpress manage document title
-  // This is literally to not use <title>
-  add_theme_support('title-tag');
-
+// Let Wordpress manage document title
+// This is literally to not use <title>
+add_theme_support('title-tag');
 
 ?>
