db17be157e52c9c352fc3ed690d722200a22126e
Xiaoming <mail@xiaoming.se>
2018-03-22 22:26:10 +0100

    Add widget area in footer

--- themes/themeone/footer.php
+++ themes/themeone/footer.php
@@ -3,10 +3,19 @@
 <footer class="site-footer">
 
   <div class="grid-container">
-    <?php
-      get_template_part('template-parts/footer_nav');
-    ?>
-    <ul>
+    <div class="grid-x grid-margin-x">
+      <div class="cell medium-6 small-12">
+        <?php
+          get_template_part('template-parts/footer_nav');
+        ?>
+      </div>
+      <div class="cell medium-6 small-12">
+        <?php
+        get_template_part('template-parts/footer_widgets');
+        ?>
+      </div>
+    </div>
+    <ul id="theme-info-list">
       <li>E-Mail: <a href="mailto:mail@xiaoming.se">mail@xiaoming.se</a></li>
       <li>Source code can be found in <a href="https://github.com/Xiaoming94/wordpress-theme"></a> </li>
     </ul>
--- themes/themeone/functions.php
+++ themes/themeone/functions.php
@@ -27,9 +27,19 @@ function create_sidebar() {
   $sidebar_args = array (
     'name'          => __('Widgets Sidebar', 'themeone'),
     'id'            => 'sidebar-1',
-    'description'   => __('Widgets to be displayed on the index page','themeone')
+    'description'   => __('Widgets to be displayed on the index page and posts','themeone')
   );
   register_sidebar($sidebar_args);
+}
+
+// Creating footer widget area
+function create_footer_widgets() {
+  $footer_widget_args = array (
+    'name'          => __('Footer Widgets', 'themeone'),
+    'id'            => 'footer-sidebar-1',
+    'description'   => __('Widgets to be displayed in the footer', 'themeone')
+  );
+  register_sidebar($footer_widget_args);
 }
 
 function show_default_nav() {
@@ -48,6 +58,7 @@ add_action('wp_enqueue_scripts', 'themeone_init_assets');
 // Load the sidebar to the theme
 add_action('widgets_init','create_sidebar');
 
+add_action('widgets_init','create_footer_widgets');
 // Let Wordpress manage document title
 // This is literally to not use <title>
 add_theme_support('title-tag');
