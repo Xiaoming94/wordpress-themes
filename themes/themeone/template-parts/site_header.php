7429e0b81dfebb29642a6ad69a417df9834af100
Xiaoming <mail@xiaoming.se>
2018-03-24 17:05:48 +0100

    Update html for site_header.php to use more layers of Divs to wrap the custom_image

--- themes/themeone/template-parts/site_header.php
+++ themes/themeone/template-parts/site_header.php
@@ -1,14 +1,19 @@
 <!-- Header Link file -->
 
-<div class="header-image">
-  <?php
+<div class="custom-site-header">
+  <div class="header-image-media">
+    <?php
     if ( get_header_image() ) :
 
       $custom_header = get_custom_header();
       $img_alt = get_bloginfo( 'name', 'display' );
       ?>
       <img src="<?php header_image(); ?>"
-           alt="<?php echo esc_attr( $img_alt ); ?>"' />
-    <?php
-  endif; ?>
+      alt="<?php echo esc_attr( $img_alt ); ?>"
+      sizes="100vw"
+      width="<?php echo absint( $custom_header->width )?>"
+      height="<?php echo absint( $custom_header->height )?>"/>
+      <?php
+    endif; ?>
+  </div>
 </div>
