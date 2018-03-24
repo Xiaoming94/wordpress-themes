7cec20742cfb77021135e91585f01b23878f7c71
Xiaoming <mail@xiaoming.se>
2018-03-25 00:30:05 +0100

    Add Authors page

--- themes/themeone/author.php
+++ themes/themeone/author.php
@@ -31,7 +31,7 @@
             <h2 class="author-bio-title">Author's bio</h2>
             <?php the_author_meta( 'description' ); ?>
             <hr />
-            <h3>Contact information:</h3>
+            <h2 class="author-bio-title">Contact information:</h3>
             <ul class="contact-info-list">
               <li>
                 <b>Email:</b> <?php the_author_meta( 'user_email' ); ?>
