2016eccde1ae0a3cef66de31d2da0632fa8b9590
Xiaoming <mail@xiaoming.se>
2018-03-24 11:27:49 +0100

    remove style elements

--- themes/themeone/template-parts/site_header.php
+++ themes/themeone/template-parts/site_header.php
@@ -8,7 +8,6 @@
       $img_alt = get_bloginfo( 'name', 'display' );
       ?>
       <img src="<?php header_image(); ?>"
-           style="width:<?php echo absint( $custom_header->width ); ?>px;height:<?php echo absint( $custom_header->height ); ?>px;"
            alt="<?php echo esc_attr( $img_alt ); ?>"' />
     <?php
   endif; ?>
