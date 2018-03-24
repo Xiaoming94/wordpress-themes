050294541d5c4d1953536dc06740a27e8356fd52
Xiaoming <mail@xiaoming.se>
2018-03-24 23:06:46 +0100

    Display author and post date in the bottom of preview

--- themes/themeone/template-parts/blog_index.php
+++ themes/themeone/template-parts/blog_index.php
@@ -13,24 +13,29 @@
           edit_post_link( __("[Edit Post]"),"  ","  ",get_the_ID(),"edit-post-link" );
         ?>
       </h3>
-      <div class="post-content-preview grid-x grid-margin-x">
+        <div class="post-content-preview grid-x grid-margin-x">
 
-        <?php
-        if ( has_post_thumbnail() || post_have_image( get_the_ID() ) ):
-          ?>
-          <div class="post-thumbnail-container cell medium-4 small-12">
-            <?php show_post_image( get_the_ID() ) ?>
-          </div>
-          <div class="cell medium-8 small-12 post-preview-margin">
-            <?php show_post_content_preview( get_the_content(), get_the_permalink() ); ?>
-          </div>
           <?php
-        else:
-           ?>
-           <div class="cell">
-             <?php show_post_content_preview( get_the_content(), get_the_permalink() ); ?>
-           </div>
-        <?php endif; ?>
+          if ( has_post_thumbnail() || post_have_image( get_the_ID() ) ):
+            ?>
+            <div class="post-thumbnail-container cell medium-4 small-12">
+              <?php show_post_image( get_the_ID() ) ?>
+            </div>
+            <div class="cell medium-8 small-12 post-preview-margin">
+              <?php show_post_content_preview( get_the_content(), get_the_permalink() ); ?>
+            </div>
+            <?php
+          else:
+            ?>
+            <div class="cell">
+              <?php show_post_content_preview( get_the_content(), get_the_permalink() ); ?>
+            </div>
+          <?php endif; ?>
+
+      </div>
+      <div class="post-meta-bar">
+        Posted on <?php echo get_the_date() ?> by
+        <?php the_author_posts_link();?>
       </div>
       <?php
       endwhile;
