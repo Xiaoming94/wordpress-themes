541b18063c428a54515d9c639f10876ee0242d01
Xiaoming <mail@xiaoming.se>
2018-03-26 16:59:16 +0200

    Add card-section and card to post-meta-bar in single

--- themes/themeone/single.php
+++ themes/themeone/single.php
@@ -25,9 +25,11 @@
           ?>
 
           <!-- Showing The Author and date -->
-          <div class="post-meta-bar">
-            This post is written by <?php the_author_posts_link(); ?>
-            on <?php echo get_the_date(); ?>
+          <div class="post-meta-bar card">
+            <div class="card-section">
+              This post is written by <?php the_author_posts_link(); ?>
+              on <?php echo get_the_date(); ?>
+            </div>
           </div>
 
           <?php
