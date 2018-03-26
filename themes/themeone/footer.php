acc8850fc13ffef1a1d1f5f84af8b30f386b592e
Xiaoming <mail@xiaoming.se>
2018-03-26 21:14:58 +0200

    Show link to github repo in footer

--- themes/themeone/footer.php
+++ themes/themeone/footer.php
@@ -17,7 +17,7 @@
     </div>
     <ul id="theme-info-list">
       <li>E-Mail: <a href="mailto:mail@xiaoming.se">mail@xiaoming.se</a></li>
-      <li>Source code can be found in <a href="https://github.com/Xiaoming94/wordpress-theme"></a> </li>
+      <li>Source code can be found at <a href="https://github.com/Xiaoming94/wordpress-theme">https://github.com/Xiaoming94/wordpress-theme</a> </li>
     </ul>
     Wordpress theme by XiaoMing
     &copy; Henry Yang <a href="https://xiaoming.se">(XiaoMing)</a> - 2017
