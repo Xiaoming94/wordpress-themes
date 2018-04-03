c3b538ee245106433521f3d2c37020a8f780ad34
Xiaoming <mail@xiaoming.se>
2018-04-03 15:05:28 +0200

    Change from 2column widgets to 3columns

--- themes/themeone/functions.php
+++ themes/themeone/functions.php
@@ -40,7 +40,7 @@ function create_footer_widgets() {
     'name'          => __('Footer Widgets', 'themeone'),
     'id'            => 'footer-sidebar-1',
     'description'   => __('Widgets to be displayed in the footer', 'themeone'),
-    'before_widget' => '<div class="cell medium-6 small-12 widget">',
+    'before_widget' => '<div class="cell medium-4 small-12 widget">',
     'after_widget'  => '</div>'
   );
   register_sidebar($footer_widget_args);
