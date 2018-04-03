dbb3c619333b39d22049fab1b97cf0f35f2b7b16
Xiaoming <mail@xiaoming.se>
2018-04-03 21:41:02 +0200

    Add 404.php

---
+++ themes/themeone/404.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 404.php template of themeone, Wordpress theme by XiaoMing
+ * The content of this file is displayed upon error 404.
+ *
+ * @link https://github.com/Xiaoming94/wordpress-theme/themes/themeone
+ * @version 0.1.0
+ * @author XiaoMing
+ */
+
+get_header();
+?>
+
+<h2 class="page-title">404 Page Not Found</h2>
+<hr>
+
+The content you are searching for doesn't exist or have been removed.
+
+Are you sure it should? than don't hesitate to contact me on <a href="mailto:henyan94@gmail.com">henyan94@gmail.com</a>
+
+Sorry for the inconvenience
+<?php
+get_footer();
+?>
